5f93096320fcb990cc3ed5a54cb67f9413fda32e
Enzo Enrico <e.enrico2005@gmail.com>
2025-01-08 20:33:37 -0300

    fixed route for api call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,7 +35,7 @@ export const Home = () => {
     setActive(true)
     try {
       console.log(userInput)
-      const response = await fetch(`http://localhost:3000/user/${userInput}`, {
+      const response = await fetch(`http://127.0.0.1:3000/user/${userInput}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
